71df953105ee2c2edc1bbbfa1285834220cc529c
Shikha Jain <sj023279@gmail.com>
2024-02-23 13:00:32 +0530

    worked on my lead page

--- src/screens/MyLeads.jsx
+++ src/screens/MyLeads.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { images } from "../components/Images";
 
 function MyLeads() {
-
   return (
     <>
       <div className="content-page">
@@ -17,21 +16,53 @@ function MyLeads() {
               <h4 className="page-title">My Leads</h4>
             </div>
             <div className="lead-card">
-              <img src={images.avatar_1} />
-              <div className="lead-card-data">
-                <h2>Kaye Morris</h2>
-                <h3>UX Developer</h3>
-                <p>
-                  Empowering users through captivating interfaces, turning ideas
-                  into pixel-perfect realities.
-                </p>
-                <button>Follow Account</button>
+              <div className="row">
+                <div className="col-12 col-md-5 col-lg-4">
+                  <div className="left bg-primary">
+                    <ul className="m-0 p-0">
+                      <li>
+                        Name: <span>Sanju</span>
+                      </li>
+                      <li>
+                        Email: <span>Sanju@gmail.com</span>
+                      </li>
+                      <li>
+                        Phone no: <span>9753568643</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-12 col-md-7 col-lg-8">
+                  <div className="lead-card-data">
+                    <h2>Create a Lead</h2>
+                    <form action="#" className="row">
+                      <div className="col-12 col-md-6 mb-3">
+                        <label className="form-label">First Name</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="col-12 col-md-6 mb-3">
+                        <label className="form-label">Last Name</label>
+                        <input type="text" className="form-control" />
+                      </div>
+                      <div className="col-12 col-md-6 mb-3">
+                        <label className="form-label">Phone no.</label>
+                        <input type="number" className="form-control" />
+                      </div>
+                      <div className="col-12 col-md-6 mb-3">
+                        <label className="form-label">Email</label>
+                        <input type="email" className="form-control" />
+                      </div>
+                      <div className="col-12">
+                        <button className="btn btn-purple">Submit</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-     
     </>
   );
 }
