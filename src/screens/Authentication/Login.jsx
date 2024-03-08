7b8e8f1edf164c0b84263c72e1e9c1cd8d44e85e
Shikha Jain <sj023279@gmail.com>
2024-03-08 15:48:06 +0530

    added toast in login page

--- src/screens/Authentication/Login.jsx
+++ src/screens/Authentication/Login.jsx
@@ -4,10 +4,14 @@ import { images } from "../../components/Images";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import useAuthStore from "../../store/authStore";
 import { apis } from "../../utils/URL";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 
 function Login() {
   const navigate = useNavigate();
   const { token, setToken } = useAuthStore();
+  const [showToast, setShowToast] = useState(false);
+  const [loginStatus, setLoginStatus] = useState();
   console.log(token);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -19,6 +23,10 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false);
+    }, 2650);
     handleLogin();
   };
 
@@ -35,139 +43,160 @@ function Login() {
         console.log(e?.data, "res");
         localStorage.setItem("token", e?.data?.token);
         navigate(`${path ? path : "/manage-bank"}`);
+        setLoginStatus("success");
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error, "error");
+
+        setLoginStatus("failure");
       });
   };
 
   return (
-    <section className="authentication-bg position-relative">
-      <div className="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5 position-relative">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-xxl-6 col-lg-6">
-              <div className="card overflow-hidden">
-                <div className="row g-0">
-                  <div className="col-xl-12 col-lg-12 col-md-12 col-12">
-                    <div className="d-flex flex-column h-100">
-                      <div className="auth-brand px-4 pt-4 text-center">
-                        <a href="index.html" className="logo-light">
-                          <img src={images.logo_dark} alt="logo" height={22} />
-                        </a>
-                        <a href="index.html" className="logo-dark">
-                          <img
-                            src={images.logo_dark}
-                            alt="dark logo"
-                            height={52}
-                          />
-                        </a>
-                      </div>
-                      <div className="px-4 mb-4">
-                        <h4 className="fs-20">Sign In</h4>
-                        <p className="text-muted mb-3">
-                          Enter your email address and password to access
-                          account.
-                        </p>
-                        {/* form */}
-                        <form action="/" onSubmit={handleSubmit}>
-                          <div className="mb-3">
-                            <label
-                              htmlFor="emailaddress"
-                              className="form-label"
-                            >
-                              Type
-                            </label>
-                            <select className="form-select">
-                              <option value="1">Admin</option>
-                              <option value="2">Manager</option>
-                            </select>
-                          </div>
-                          <div className="mb-3">
-                            <label
-                              htmlFor="emailaddress"
-                              className="form-label"
-                            >
-                              Email address
-                            </label>
-                            <input
-                              className="form-control"
-                              type="email"
-                              id="emailaddress"
-                              placeholder="Enter your email"
-                              ref={emailRef}
-                              // value={email}
-                              // onChange={(e) => setEmail(e.target.value)}
-                              required
+    <>
+      <section className="authentication-bg position-relative">
+        <div className="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5 position-relative">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xxl-6 col-lg-6">
+                <div className="card overflow-hidden">
+                  <div className="row g-0">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-12">
+                      <div className="d-flex flex-column h-100">
+                        <div className="auth-brand px-4 pt-4 text-center">
+                          <a href="index.html" className="logo-light">
+                            <img
+                              src={images.logo_dark}
+                              alt="logo"
+                              height={22}
                             />
-                          </div>
-                          <div className="mb-3">
-                            <Link
-                              to="/forgot-password"
-                              className="text-muted float-end"
-                            >
-                              <small>Forgot your password?</small>
-                            </Link>
-                            <label htmlFor="password" className="form-label">
-                              Password
-                            </label>
-                            <input
-                              className="form-control"
-                              type="password"
-                              id="password"
-                              placeholder="Enter your password"
-                              // value={password}
-                              ref={passwordRef}
-                              // onChange={(e) => setPassword(e.target.value)}
-                              required
+                          </a>
+                          <a href="index.html" className="logo-dark">
+                            <img
+                              src={images.logo_dark}
+                              alt="dark logo"
+                              height={52}
                             />
-                          </div>
-                          <div className="mb-3">
-                            <div className="form-check">
-                              <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="checkbox-signin"
-                              />
+                          </a>
+                        </div>
+                        <div className="px-4 mb-4">
+                          <h4 className="fs-20">Sign In</h4>
+                          <p className="text-muted mb-3">
+                            Enter your email address and password to access
+                            account.
+                          </p>
+                          {/* form */}
+                          <form action="/" onSubmit={handleSubmit}>
+                            <div className="mb-3">
                               <label
-                                className="form-check-label"
-                                htmlFor="checkbox-signin"
+                                htmlFor="emailaddress"
+                                className="form-label"
                               >
-                                Remember me
+                                Type
                               </label>
+                              <select className="form-select">
+                                <option value="1">Admin</option>
+                                <option value="2">Manager</option>
+                              </select>
                             </div>
-                          </div>
-                          <div className="mb-3 text-start">
-                            <button
-                              className="btn btn-soft-primary w-100"
-                              type="submit"
-                            >
-                              <i className="ri-login-circle-fill me-1" />
-                              <span className="fw-bold">Log In</span>
-                            </button>
-                          </div>
-                          <div className="mb-0 text-start">
-                            <p className="text-muted text-center mb-3">
-                              Don't have an account?{" "}
-                              <Link to="/register">Register here</Link>
-                            </p>
-                          </div>
-                        </form>
-                        {/* end form*/}
+                            <div className="mb-3">
+                              <label
+                                htmlFor="emailaddress"
+                                className="form-label"
+                              >
+                                Email address
+                              </label>
+                              <input
+                                className="form-control"
+                                type="email"
+                                id="emailaddress"
+                                placeholder="Enter your email"
+                                ref={emailRef}
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
+                                required
+                              />
+                            </div>
+                            <div className="mb-3">
+                              <Link
+                                to="/forgot-password"
+                                className="text-muted float-end"
+                              >
+                                <small>Forgot your password?</small>
+                              </Link>
+                              <label htmlFor="password" className="form-label">
+                                Password
+                              </label>
+                              <input
+                                className="form-control"
+                                type="password"
+                                id="password"
+                                placeholder="Enter your password"
+                                // value={password}
+                                ref={passwordRef}
+                                // onChange={(e) => setPassword(e.target.value)}
+                                required
+                              />
+                            </div>
+                            <div className="mb-3">
+                              <div className="form-check">
+                                <input
+                                  type="checkbox"
+                                  className="form-check-input"
+                                  id="checkbox-signin"
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="checkbox-signin"
+                                >
+                                  Remember me
+                                </label>
+                              </div>
+                            </div>
+                            <div className="mb-3 text-start">
+                              <button
+                                className="btn btn-soft-primary w-100"
+                                type="submit"
+                              >
+                                <i className="ri-login-circle-fill me-1" />
+                                <span className="fw-bold">Log In</span>
+                              </button>
+                            </div>
+                            <div className="mb-0 text-start">
+                              <p className="text-muted text-center mb-3">
+                                Don't have an account?{" "}
+                                <Link to="/register">Register here</Link>
+                              </p>
+                            </div>
+                          </form>
+                          {/* end form*/}
+                        </div>
                       </div>
                     </div>
+                    {/* end col */}
                   </div>
-                  {/* end col */}
                 </div>
               </div>
+              {/* end row */}
             </div>
             {/* end row */}
           </div>
-          {/* end row */}
+          {/* end container */}
         </div>
-        {/* end container */}
-      </div>
-    </section>
+      </section>
+      <ToastContainer position="top-end" className="position-fixed">
+        <Toast show={showToast} onClose={() => setShowToast(false)}>
+          <Toast.Header>
+            <h2>Heads up!</h2>
+          </Toast.Header>
+          <Toast.Body>
+            {loginStatus === "success"
+              ? "Login Successfully"
+              : "Incorrect ID or Password"}
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </>
   );
 }
 
