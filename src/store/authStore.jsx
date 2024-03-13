943dda251974533b271c717bd5ea88e9ed7f45e8
Sahaj Arya <sahajarya@Sahajs-MacBook-Pro.local>
2024-03-13 17:54:04 +0530

    f

--- src/store/authStore.jsx
+++ src/store/authStore.jsx
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useAuthStore = create((set) => ({
   token: "",
   setToken: (data) => set({ token: data }),
+  loading: true,
+  setLoading: (data) => set({ loading: data }),
 }));
 
 export default useAuthStore;
