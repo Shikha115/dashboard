c00f812664e3ade9bc0b918cb0a21ff33d8d3d7b
Sahaj Arya <sahajarya@Sahajs-MacBook-Air.local>
2024-06-28 23:50:53 +0530

    push june 20 for apk

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./TextEditor.css"; // Import your custom CSS file
+
 function TextEditor({ item, onChange }) {
   const modules = {
     toolbar: [
@@ -10,9 +13,14 @@ function TextEditor({ item, onChange }) {
       [{ align: ["right", "center", "justify"] }],
       [{ list: "ordered" }, { list: "bullet" }],
       ["link", "image"],
+      [{ indent: "-1" }, { indent: "+1" }],
       [{ color: ["red", "#785412"] }],
       [{ background: ["red", "#785412"] }],
+      ["clean"],
     ],
+    clipboard: {
+      matchVisual: false,
+    },
   };
 
   const formats = [
@@ -34,15 +42,16 @@ function TextEditor({ item, onChange }) {
   ];
 
   return (
-    <>
+    <div className="text-editor-container">
       <ReactQuill
         theme="snow"
         modules={modules}
         formats={formats}
-        value={Array.isArray(item) ? item.join("") : item}
+        value={item}
         onChange={onChange}
+        className="text-editor"
       />
-    </>
+    </div>
   );
 }
 
